3e065e25cce837f090a819a40fe7bff00453323f
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-21 16:04:58 +0200

    Set header and input field

---
+++ app/src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Input from "./Input";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Input />
+    </div>
+  );
+};
+
+export default App;
---
+++ app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <h1 className="text-center text-2xl sm:text-3xl font-bold">
+      Search data about country
+    </h1>
+  );
+};
+
+export default Header;
