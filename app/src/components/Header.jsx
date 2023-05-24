02abfca5010c4b0b4264394c0cf98ddd83e964c7
Oskar Kuchta <oskarkuchta5@gmail.com>
2023-05-24 12:40:17 +0200

    Add some styles

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,6 +1,6 @@
-const Header = ({children}) => {
+const Header = ({style, children}) => {
   return (
-    <h1 className="text-2xl font-bold text-center text-gray-800 sm:text-3xl xl:text-4xl ">
+    <h1 className="text-2xl font-bold text-center text-gray-800 sm:text-3xl xl:text-4xl transition duration-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-green-600 ">
       {children}
     </h1>
   );
